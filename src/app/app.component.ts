d09faaf6529835870f796d3703757d682683a5f0
Onbashi-Meriem <meryemonbasi5@gmail.com>
2022-06-28 17:23:19 +0200

    of operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs';
+
+  constructor(){
+     
+    const values=of('fatih', 4,[1,2,3,4],'mehmet',4.12)
+
+    values.subscribe(data=>{
+      console.log(data)
+    })
+  }
 }
