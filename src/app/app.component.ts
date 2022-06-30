914fce91b9e14082fb9f7bbd1e09759b728e5995
Onbashi-Meriem <meryemonbasi5@gmail.com>
2022-06-30 10:39:29 +0200

    timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,50 @@
 import { Component } from '@angular/core';
-import { interval, of } from 'rxjs';
+import { interval, Observable, of, Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'rxjs';
+  subscription: Subscription;
 
-  constructor(){
-     
+  constructor() {
+    //--------of----------
+
     // const values=of('fatih', 4,[1,2,3,4],'mehmet',4.12)
 
     // values.subscribe(data=>{
     //   console.log(data)
     // })
 
-    const publisher=interval(1000)
+    // -------------interval----------
 
-    publisher.subscribe(val=>{
-      console.log("data",val)
-    },err=>{},()=>{
-      console.log('islem bitti')
-    })
+    // const publisher=interval(1000)
+
+    // publisher.subscribe(val=>{
+    //   console.log("data",val)
+    // },err=>{},()=>{
+    //   console.log('islem bitti')
+    // })
+
+    // ----------timer----------
+
+    const publisher = timer(5000, 1000);
+
+    this.subscription = publisher.subscribe(
+      (val) => {
+        console.log('Bu islem sayfada 5 sn sonra görünecek =' + val);
+      },
+      (err) => {},
+      () => {
+        console.log('islem tamamlandi');
+      }
+    );
+  }
+
+  stop() {
+    this.subscription.unsubscribe();
   }
 }
