fb6ef93fee86aa9006813c15b7013f50c9fe4118
Onbashi-Meriem <meryemonbasi5@gmail.com>
2022-07-01 14:02:09 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import {
   timer,
   Observer,
   fromEvent,
+  first,
 } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
@@ -18,146 +19,24 @@ import { ajax } from 'rxjs/ajax';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit  {
+export class AppComponent {
   title = 'rxjs';
   subscription: Subscription;
-  @ViewChild('btn') btn:ElementRef
-
-  
 
   constructor() {
-    //--------of----------
+    // -------------first-------
 
-    // const values=of('fatih', 4,[1,2,3,4],'mehmet',4.12)
+    const myArray = from([5, 10, 15, 20, 30, 40, 300, 600]);
+    myArray.subscribe((val) => {
+      console.log('ohne first()=>' + val);
+    });
 
-    // values.subscribe(data=>{
-    //   console.log(data)
-    // })
+    myArray.pipe(first()).subscribe((val) => {
+      console.log('fist()=>', val);
+    });
 
-    // -------------interval----------
-
-    // const publisher=interval(1000)
-
-    // publisher.subscribe(val=>{
-    //   console.log("data",val)
-    // },err=>{},()=>{
-    //   console.log('islem bitti')
-    // })
-
-    // ----------timer----------
-
-    //   const publisher = timer(5000, 1000);
-
-    //   this.subscription = publisher.subscribe(
-    //     (val) => {
-    //       console.log('Bu islem sayfada 5 sn sonra görünecek =' + val);
-    //     },
-    //     (err) => {},
-    //     () => {
-    //       console.log('islem tamamlandi');
-    //     }
-    //   );
-
-    // -----------range----------
-
-    // let publisher=range(0,20)
-
-    // publisher.subscribe((val)=>{
-    //   console.log(val)
-
-    // },err=>{},()=>{
-    //   console.log('islem tamamlandi')
-    // })
-
-    // ----------from-----------
-
-    //   const map=new Map();
-
-    //   map.set(1,'Kitaplar');
-    //   map.set(  2,'Kalemler');
-    //   map.set(  3,'Silgiler');
-
-    //   const publisher=from(map)
-
-    // this.subscription=publisher.subscribe((val)=>{
-
-    //   console.log(`${val[0]} = ${val[1]}`);
-    //   console.log(val);
-    //   console.log(typeof val, typeof `${val}`)
-    //   console.log(`${val}`);
-
-    // },err=>{},()=>{
-    //   console.log('islem tamamlandi');
-
-    // })
-
-    // ------------defer---------
-
-    // const publisher=of(new Date())
-    // const publisher2=defer(()=>of(new Date()))
-
-    // const myTimer=timer(3000)
-
-    // myTimer.subscribe((val)=>{
-    // publisher.subscribe(date1=>{
-    //   console.log('date without defer', date1,)
-    // })
-    // publisher2.subscribe(date2=>{
-    //   console.log('date with defer', date2,)
-    // })
-    // },()=>{
-    //   console.log('islem tamamlandi');
-
-    // })
-
-    // ----------------ajax--------------
-
-    // const ajaxRequest = ajax.getJSON(
-    //   'https://jsonplaceholder.typicode.com/todos/1'
-    // );
-    // ajaxRequest.subscribe(
-    //   (data) => {
-    //     console.table(data);
-    //   },
-    //   (err) => {
-    //     console.log(err.message);
-    //   },
-    //   () => {
-    //     console.log('ajax request tamamlandi');
-    //   }
-    // );
-
-    // -------------create-----------------
-
-    // const myObservable = Observable.create((observer: Observer<any>) => {
-    //   observer.next('meriem onbashi ugurlu');
-    //   observer.next('Name Surname');
-    //   observer.complete();
-    // });
-
-    // myObservable.subscribe(
-    //   (data: any) => {
-    //     console.log(data);
-    //   },
-    //   () => {
-    //     console.log('islem tamamlandi');
-    //   }
-    // );
-
-   
-
-
-  }
-  ngAfterViewInit(): void {
-     // ------------fromEvent--------------
-
-    fromEvent(this.btn.nativeElement, 'click').subscribe(data=>{
-      console.log(data)
-    })
-  }
-  stop() {
-    // this.subscription.unsubscribe();
+    myArray.pipe(first((val) => val > 20)).subscribe((val) => {
+      console.log('first >20=>', val);
+    });
   }
 }
-
-
